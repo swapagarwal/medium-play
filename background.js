a239b040b5ca96d68b8b22dcfcdc68afe24b2d39
Liam McDermott <liam.hj.mcdermott@gmail.com>
2019-05-20 00:07:28 -0400

    Play same tab

--- background.js
+++ background.js
@@ -14,27 +14,7 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true
-  }, function(array_of_tabs) {
-    var tab = array_of_tabs[0];
-    var url = tab.url;
-    var id = url.split("-").pop();
-    if (undefined !== id) {
-      var request = new XMLHttpRequest();
-      request.open("GET", "https://medium.com/p/" + id + "/notes", true);
-      request.onload = function() {
-        if (request.status === 200) {
-          var resp = request.responseText;
-          var match = resp.match(/\"audioVersionUrl\":\"([^"]+)\"/);
-          if (null !== match && null !== match[1]) {
-            var audioUrl = match[1];
-            chrome.tabs.create({"url": audioUrl});
-          }
-        }
-      };
-      request.send();
-    }
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {"message": "medium-play"});
   });
 });
