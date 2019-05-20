8370b3094de0ca611d7db3a50cb0a7874ac6609c
Liam McDermott <liam.hj.mcdermott@gmail.com>
2019-05-20 00:10:46 -0400

    Clean up content.js

--- content.js
+++ content.js
@@ -4,11 +4,8 @@ chrome.runtime.onMessage.addListener(
 			var text = document.body.textContent;
 			var match = text.match(/\"audioVersionUrl\":\"([^"]+)\"/);
 			if (null !== match && null !== match[1]) {
-				var audio = new Audio(match[1]);
-				//audio.play();
-					
-				var x = document.getElementsByClassName("elevateAudioPlayer");
-				x[0].innerHTML = '<audio preload="auto" controls="controls" src=' + match[1] + '></audio>'; 
+				var audioPlayer = document.getElementsByClassName("elevateAudioPlayer");
+				audioPlayer[0].innerHTML = '<audio preload="auto" controls="controls" src=' + match[1] + '></audio>'; 
 			}
     	}
 });
